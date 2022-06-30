dfc21f01981196c3512aa0a51bb82ad7cc356d97
AC0xRPFS001 <ac0xrpfs001@vivaldi.net>
2022-06-30 14:59:32 +0000

    feature: implement the subtract with carry random generator

--- src/constant.ts
+++ src/constant.ts
@@ -5,3 +5,9 @@ export const LCG_MAGIC_A = [103, 5, 1025, 9, 13, 217, 433, 93];
 export const LCG_MAGIC_B = [51, 17, 119, 21, 773, 29, 11, 87, 63];
 
 export const _32Bits = 2**32 - 1;
+export const _2to32 = 2 ** 32;
+
+export const SWC_LAGS: Array<[number, number]> = [
+    [24, 55], [38, 89], [128, 159], [97, 127], [353, 521], 
+    [168, 521], [83, 258], [107, 378], [273, 607], [418, 1279]
+]
---
+++ src/generator/swc.ts
@@ -0,0 +1,39 @@
+import { RandomGenerator } from './base';
+import { SWC_LAGS, _2to32 } from '../constant';
+import { MT19937 } from './mt19937';
+
+
+export class SubtractWithCarry extends RandomGenerator<number> {
+    protected nowValue: number;
+    private lags: [number, number];
+    private sequence: number[];
+    private carry: number = 0;
+
+    constructor(seed: number) {
+        super(seed);
+        this.lags = SWC_LAGS[(seed >>> 3) % SWC_LAGS.length];
+        this.init();
+    }
+
+    private init(): void {
+        const initValueGen = new MT19937(this.seed >>> 3);
+        const diff = this.lags[1] - this.lags[0] + 1;
+        this.sequence = new Array(diff);
+
+        for (let i = 0; i < diff; ++i) {
+            this.sequence[i] = initValueGen.randomNumber.next().value;
+        }
+
+        if (this.sequence[this.sequence.length - 1] < 0) this.carry = 1;
+    }
+
+    protected algorithm(): void {
+        const v = (this.sequence[0] - this.sequence[this.sequence.length - 1] - this.carry) % _2to32;
+        this.carry = v < 0 ? 1 : 0;
+
+        this.sequence.shift()
+        this.sequence.push(v);
+
+        this.nowValue = v;
+    }
+}
