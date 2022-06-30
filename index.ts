da21ef100506cbf8e8d9383b498e99a66f8dcb4d
AC0xRPFS001 <ac0xrpfs001@vivaldi.net>
2022-06-30 13:16:11 +0000

    chore: add missed export

--- index.ts
+++ index.ts
@@ -2,5 +2,5 @@ export { LCG } from './src/generator/lcg';
 export { MT19937 } from './src/generator/mt19937';
 export { IRandomGenerator, RandomGenerator } from './src/generator/base';
 
-export { BaseDistributionMachine } from './src/distribution/base';
+export { IDistrbutionMachine, BaseDistributionMachine } from './src/distribution/base';
 export { LinearDistribution } from './src/distribution/linear';
