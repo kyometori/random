ece4a58b8c5ff3fcab7c896bbdbe68fe6cef3132
AC0xRPFS001 <ac0xrpfs001@vivaldi.net>
2022-06-30 17:16:22 +0000

    feature: implement an utility class to produce random float number

--- src/utils/float.ts
+++ src/utils/float.ts
@@ -1,24 +1,22 @@
-import { RandomGenerator } from './base';
-import { MT19937 } from './mt19937';
+import type { RandomGenerator } from '../generator/base';
+import { MT19937 } from '../generator/mt19937';
 
-export class RandomFloat extends RandomGenerator<number> {
-    protected nowValue: number;
+export class RandomFloat {
     private internalGenerator: RandomGenerator<number>;
 
     constructor(seed: number) {
-        super(seed);
         this.internalGenerator = new MT19937(seed);
     }
 
-    protected algorithm(): void {
-        const N = 23;
+    public getValue(): number {
+        const N = 30;
 
         let result = 0;
         for (let i = 2; i < N; ++i) {
             result += this.internalGenerator.randomNumber.next().value / i;
         }
 
-        this.nowValue = Math.abs(result % 1);
+        return Math.abs(result % 1);
     }
 
 }
