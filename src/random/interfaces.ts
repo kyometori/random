bbd5c3b6bc8d38a31b3cae51f2726dda5a441024
AC0xRPFS001 <ac0xrpfs001@vivaldi.net>
2026-09-24 08:34:52 +0800

    feature: map c++ typings to ts

---
+++ src/random/interfaces.ts
@@ -0,0 +1,11 @@
+import type { UInt, SeedResult, Size } from './types';
+
+export interface SeedSequence {
+    generate(destination: SeedResult[]): void;
+    size(): Size;
+    param(): SeedResult[];
+}
+
+export interface UniformRandomBitGenerator<T extends UInt = UInt> {
+    next(): T;
+}
---
+++ src/random/types.ts
@@ -0,0 +1,17 @@
+export type UInt32 = bigint & {
+    readonly __uint32: unique symbol;
+};
+
+export type UInt64 = bigint & {
+    readonly __uint64: unique symbol;
+};
+
+export type UInt = UInt32 | UInt64;
+
+export type Real = number;
+
+export type Size = number;
+
+export type UnsignedLongLong = bigint;
+
+export type SeedResult = UInt32;
